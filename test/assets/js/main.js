76cb5e04849cb7ddb99a8728caccafef6c702f24
Sergio Téllez <sergiortellez@gmail.com>
2019-06-21 11:55:55 -0500

    removed test files, added .gitignore

--- test/assets/js/main.js
+++
@@ -1,8 +0,0 @@
-var $carousel = $('.carousel').flickity();
-
-$('.button--show').on('click', function () {
-    // show
-    $carousel.show()
-        // trigger resize method after showing
-        .flickity('resize');
-});
