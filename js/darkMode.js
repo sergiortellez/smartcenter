23411328b3e93804ff896838ffce244e67649972
sergiortellez <sergiortellez@gmail.com>
2019-10-01 00:59:43 -0500

    corrected: - scripts with errors and load time - minified photos for improved loading time
    added:
    - nigth/dark mode v1
    - team members pictures

---
+++ js/darkMode.js
@@ -0,0 +1,54 @@
+ // variables
+ var darkMode;
+ var getNav = document.getElementById("nav");
+ var getLabelSwitch = document.getElementById('lightDarkLabel');
+ var getLabelIcon = document.getElementById('lightDarkIcon');
+ //on load & listeners
+ window.onload = function () {
+     var darkMode = false;
+     console.log("darkmode: " + darkMode);
+ }
+
+ document.getElementById("toggle").addEventListener("click", toggle);
+
+ //functions
+ function toggle() {
+     if (darkMode) {
+         //toggle colors
+
+         document.documentElement.style.setProperty('--brColor', '#fff');
+         document.documentElement.style.setProperty('--textColor', '#000');
+
+         // navbar toogle
+         getNav.classList.add('navbar-light');
+         getNav.classList.remove('navbar-dark');
+         getNav.classList.add('bg-light-custom');
+         getNav.classList.remove('bg-dark');
+         //change labels
+         getLabelSwitch.innerHTML = "oscuro";
+         getLabelIcon.classList.add('fa-moon');
+         getLabelIcon.classList.remove('fa-sun');
+         //toggle status
+         darkMode = false;
+         console.log("darkmode: " + darkMode);
+
+     } else {
+         //toggle colors
+
+         document.documentElement.style.setProperty('--brColor', '#000');
+         document.documentElement.style.setProperty('--textColor', '#fff');
+
+         // navbar toogle
+         getNav.classList.add('navbar-dark');
+         getNav.classList.remove('navbar-light');
+         getNav.classList.remove('bg-light-custom');
+         getNav.classList.add('bg-dark');
+         //change labels
+         getLabelSwitch.innerHTML = "claro";
+         getLabelIcon.classList.add('fa-sun');
+         getLabelIcon.classList.remove('fa-moon');
+         //toggle status
+         darkMode = true;
+         console.log("darkmode: " + darkMode);
+     }
+ };
