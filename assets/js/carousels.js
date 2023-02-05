1b353ce36b5eb53a24e7f65335e12b7d8da94677
sergiortellez <sergiortellez@gmail.com>
2023-02-04 19:01:53 -0600

    se construyeron los playholders para la precarga del carrusel

--- assets/js/carousels.js
+++ assets/js/carousels.js
@@ -1,3 +1,46 @@
+/*<------------------------------------------------->
+<!--	creación de carruseles	-->
+<!------------------------------------------------->*/
+
+const API_KEY = "AIzaSyAY92qoSUcTrzZ669HLcAh-Mx9KBKKfePo";
+// Esta constante guarda la clave API de Google. 
+
+const PLAYLIST_ID = "PLhMMXwN8RXkwqQ7JMSyWl2JYt2B4_sU5O";
+// Esta constante guarda el ID de la lista de reproducción que se desea obtener.
+
+const endpoint = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet,contentDetails&maxResults=12&playlistId=${PLAYLIST_ID}&key=${API_KEY}`;
+// Esta constante guarda la URL de la solicitud API. Incluye el ID de la lista de reproducción y la clave API, además de establecer los detalles que se desean obtener (snippet y contentDetails) y el número máximo de resultados por solicitud (12).
+
+
+// Utiliza la función `fetch` para realizar una solicitud GET a la URL de la API.
+fetch(endpoint)
+    // Si la solicitud es exitosa, la respuesta se convierte en un objeto JSON.
+    .then(response => response.json())
+    //Luego, se extrae la información relevante de cada video(ID, título y miniatura) y se almacena en una nueva constante`videos`
+    .then(data => {
+        const videos = data.items.map(item => {
+            return {
+                id: item.snippet.resourceId.videoId,
+                title: item.snippet.title,
+                thumbnail: item.snippet.thumbnails.default.url
+            };
+        });
+    })
+    // Si ocurre un error durante la solicitud, se mostrará un mensaje de error en la consola.
+    .catch(error => {
+        console.error(error);
+    });
+
+/*<!------------------------------------------------->
+<!--	end creación de carruseles	-->
+<!------------------------------------------------->*/
+
+
+/*<------------------------------------------------->
+<!--	funcionamiento de carruseles	-->
+<!------------------------------------------------->*/
+
+
 
 //selecciona todos los carruseles de la página que tengan la clase '.carousel'
 let carousels = document.querySelectorAll(".carousel");
@@ -58,6 +101,8 @@ carousels.forEach(carousel => {
 });
 
 
+
+
 carouselItems.forEach(item => {
     let dataId = item.getAttribute('data-id');
 
@@ -65,8 +110,33 @@ carouselItems.forEach(item => {
 
     function displayVideo() {
         let src = "https://www.youtube.com/embed/" + dataId;
-        console.log(src)
+        console.log(src);
         videoPreview.src = src;
+        console.log(getVideoTitle(dataId));
+    }
 
+
+    function getVideoTitle(videoId) {
+
+        var ytApiKey = "AIzaSyAY92qoSUcTrzZ669HLcAh-Mx9KBKKfePo";
+        var videoId = videoId;
+
+        $.get("https://www.googleapis.com/youtube/v3/videos?part=snippet&id=" + videoId + "&key=" + ytApiKey, function (data) {
+            return (data.items[0].snippet.title);
+        });
     }
 });
+
+
+
+/*<!------------------------------------------------->
+<!--	end funcionamiento de carruseles	-->
+<!------------------------------------------------->*/
+
+
+
+
+
+
+
+
