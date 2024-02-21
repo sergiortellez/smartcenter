a738c1ebe15f79678af8753ba7553493490c6b50
Sergio Téllez <sergiortellez@gmail.com>
2024-02-21 16:34:38 -0600

    @app added: Navbar file component added: footer file component added: react-router-dom files

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
+//React
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+//Components
 import './App.css'
 
 function App() {
@@ -8,29 +8,7 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-        <i className="fas fa-coffee"></i> {/* Example of a solid icon */}
-        <i className="far fa-user"></i> {/* Example of a regular icon */}
-        <i className="fab fa-github"></i> {/* Example of a brand icon */}
-      </p>
+
     </>
   )
 }
