1ef99644a8be57213795eef450238b4c21c6a269
Sergio Téllez <sergiortellez@gmail.com>
2024-07-26 20:15:54 -0600

    @rellax_control, fixed: now the script runs in page load and in every resize.

--- assets/js/rellax_control.js
+++ assets/js/rellax_control.js
@@ -1,24 +1,21 @@
-document.addEventListener('DOMContentLoaded', function () {
-    function updateParallaxAttributes() {
-        const element = document.getElementById('presenciales_rellax');
-        const screenWidth = window.innerWidth;
+updateParallaxAttributes();
 
-        if (screenWidth >= 1024.1) { // For large screens
-            element.setAttribute('data-rellax-speed', '8');
-            element.setAttribute('data-rellax-percentage', '0');
-        } else if (screenWidth >= 768) { // For medium screens
-            element.setAttribute('data-rellax-speed', '5');
-            element.setAttribute('data-rellax-percentage', '0');
-        } else { // For small screens
-            element.setAttribute('data-rellax-speed', '2');
-            element.setAttribute('data-rellax-percentage', '0');
-        }
+function updateParallaxAttributes() {
+    const element = document.getElementById('presenciales_rellax');
+    const screenWidth = window.innerWidth;
+
+    if (screenWidth >= 1024.1) { // For large screens
+        element.setAttribute('data-rellax-speed', '8');
+        element.setAttribute('data-rellax-percentage', '0');
+    } else if (screenWidth >= 768) { // For medium screens
+        element.setAttribute('data-rellax-speed', '5');
+        element.setAttribute('data-rellax-percentage', '0');
+    } else { // For small screens
+        element.setAttribute('data-rellax-speed', '2');
+        element.setAttribute('data-rellax-percentage', '0');
     }
+    console.log('updateParallaxAttributes');
+}
 
-    // Initial setup
-    updateParallaxAttributes();
-
-    // Update attributes on window resize
-    window.addEventListener('resize', updateParallaxAttributes);
-
-});
+// Update attributes on window resize
+window.addEventListener('resize', updateParallaxAttributes);
