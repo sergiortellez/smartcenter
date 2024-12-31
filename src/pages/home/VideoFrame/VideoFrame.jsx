837f28682925ae66c2e878002c1128d3d1d17030
Sergio Téllez <sergiortellez@gmail.com>
2024-12-30 23:12:48 -0600

    @public, added: now videos are located in the public folder

--- src/pages/home/VideoFrame/VideoFrame.jsx
+++ src/pages/home/VideoFrame/VideoFrame.jsx
@@ -12,11 +12,31 @@
 <!------------------------------------------------->*/
 //imports
 
-export default function VideoFrame() {
-    return (
-        <>
+//styles
+import styles from './VideoFrame.module.css';
 
-        </>
+//fontAwesome
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faQuoteLeft, faQuoteRight } from '@awesome.me/kit-c09e05c963/icons/sharp-duotone/solid'
+
+
+
+export default function VideoFrame() {
+
+
+    return (
+        <div className={styles.videoFrame} >
+            <video playsInline autoPlay muted loop>
+                {/* add source here */}
+            </video>
+            <div className={styles.videoHeader} >
+                <p>
+                    <FontAwesomeIcon icon={faQuoteLeft} />
+                    Construye tu mejor versión
+                    <FontAwesomeIcon icon={faQuoteRight} />
+                </p>
+            </div>
+        </div>
     );
 }
 
