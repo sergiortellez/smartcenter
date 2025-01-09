58b37419b2c38f9c4aaefd5ec2d1d675e57a8a60
Sergio Téllez <sergiortellez@gmail.com>
2025-01-08 19:45:44 -0600

    @project, added: paths in vite config

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,18 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0' // This exposes the server to the local network
+  },
+  resolve: {
+    alias: {
+
+      '@assets': '/src/assets',
+      '@components': '/src/components',
+      '@context': '/src/context',
+      '@hooks': '/src/hooks',
+      '@data': '/src/data',
+      '@pages': '/src/pages',
+      '@firebase': '/src/firebase',
+
+    }
   }
 })
