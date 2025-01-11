b1183a60ed7e6b21a52428d98059b18c8b3bc585
Sergio Téllez <sergiortellez@gmail.com>
2025-01-11 15:08:16 -0600

    @Info(component), added:     - Files, styles and logic to render the Info component inside the WhoAreWe grid.

---
+++ src/pages/home/WhoAreWe/Info/Info.jsx
@@ -0,0 +1,31 @@
+/*<---------------------------------------------------------------------------->
+<!--	Info (Component)	-->
+<!----------------------------------------------------------------------------->
+* Description:
+     Simple component that displays a card with paragraph with a message.
+* Parameters:
+    - none
+* Dependencies:
+    - styles: Info.module.css
+* Returns/results: Displays the card itself.
+<!------------------------------------------------->*/
+
+
+
+//styles
+import styles from './Info.module.css'
+
+
+export default function Info() {
+  return (
+    <article className={styles.info}>
+      <p>Te ayudamos a ser un <span className={styles.highlight} >profesionista</span> que cumple las demandas sociales, impulsándote como <span className={styles.highlightBold} >líder</span> que <span className={styles.highlightRed} >contribuye</span> a la mejora de México y el mundo.
+      </p>
+    </article>
+  );
+}
+
+/*<!--------------------------------------------------------------------------->
+<!--	end Info(Component)	-->
+<!--------------------------------------------------------------------------->*/
+
--- src/pages/home/WhoAreWe/WhoAreWe.jsx
+++ src/pages/home/WhoAreWe/WhoAreWe.jsx
@@ -18,15 +18,16 @@ import styles from './WhoAreWe.module.css'
 
 //cards
 import Cover from './Cover/Cover';
+import Info from './Info/Info';
 
 
 export default function WhoAreWe() {
   return (
-    <section id="somos">
+    <section id="somos" className={styles.whoAreWe}>
       <h1 className={styles.title} >¿Quiénes somos?</h1>
       <div className={styles.grid}>
         <Cover />
-
+        <Info />
       </div>
     </section >
   );
