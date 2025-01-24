a2c1ef10b1726ce93dd6a975a584b2ee5b2cb171
Sergio Téllez <sergiortellez@gmail.com>
2025-01-23 21:14:22 -0600

    commit for build

--- src/pages/experiencias/Experiencias.jsx
+++ src/pages/experiencias/Experiencias.jsx
@@ -1,7 +1,53 @@
+/*<------------------------------------------------->
+<!--	Experiencias(page)	-->
+<!-------------------------------------------------->
+* Description:
+
+* Parameters:
+    -
+* Dependencies:
+* Returns/results:
+<!------------------------------------------------->*/
+
+//imports
+
+//components
+import SelfContainedExpandableCard from '@components/SelfContainedExpandableCard/SelfContainedExpandableCard'
+
+//styles
+import styles from './Experiencias.module.css'
 
 
 export default function Experiencias() {
+
+
+    /*<------------------------------------------------->
+        <!--	render logic	-->
+        <!------------------------------------------------->*/
     return (
-        <div>Experiencias</div>
+        <section className={styles.experiences} >
+
+            <h1 className={styles.title} >Nuestras experiencias</h1>
+
+            <div className={styles.grid} >
+                <SelfContainedExpandableCard cardId='first' />
+                <SelfContainedExpandableCard cardId='second' />
+                <SelfContainedExpandableCard cardId='third' />
+                <SelfContainedExpandableCard cardId='fourth' />
+                <SelfContainedExpandableCard cardId='fifth' />
+                <SelfContainedExpandableCard cardId='sixth' />
+
+
+            </div>
+        </section>
     )
+    /*<!------------------------------------------------->
+    <!--	end render logic	-->
+    <!------------------------------------------------->*/
+
 }
+
+
+/*<!------------------------------------------------->
+<!--	end Experiencias(page)	-->
+<!------------------------------------------------->*/
