b79e91d15b7fe776b6764c7255b01b7ec9f94734
Sergio Téllez <sergiortellez@gmail.com>
2025-01-30 17:53:21 -0600

    @counselingData(data), updated:         - added service of "simulador entrevista laboral"         - added file for service "apoyo psicopedagógico a deportistas"

--- src/pages/home/Experiences/buttons/Counseling/CounselingData.jsx
+++ src/pages/home/Experiences/buttons/Counseling/CounselingData.jsx
@@ -15,6 +15,11 @@
 import holaDiez from '@assets/intercampi/counseling/hola_diez_icon.webp';
 import sos from '@assets/intercampi/counseling/sos_icon.webp';
 import talent from '@assets/intercampi/counseling/talent_icon.webp';
+import simulador from '@assets/intercampi/counseling/simulador_entrevista.webp';
+import reencuadre from '@assets/intercampi/counseling/reencuadre_emocional.webp';
+import tutorias from '@assets/intercampi/counseling/tutorias_academicas.webp';
+import deportistas from '@assets/intercampi/counseling/deportistas.webp';
+
 
 
 export const CounselingData = [
@@ -24,7 +29,7 @@ export const CounselingData = [
         title: '¡Hola Diez! Adiós estrés.',
         subtitle: 'La universidad no tiene que ser estresante',
         description: 'Identifica lo que haces bien y lo que hace falta para que tengas el mejor rendimiento. Si necesitas un plan personalizado de estrategias y hábitos de estudio para tener éxito en tu carrera, este programa es para ti.',
-        campi: ['AGS', 'CDMX', 'GDL']
+        campi: ['AGS', 'CDMX']
     },
     {
         id: 'sos',
@@ -41,7 +46,15 @@ export const CounselingData = [
         title: 'Talent & Career',
         subtitle: 'Descubre tus superpoderes',
         description: '¿Tienes mucho potencial dentro de ti? ¿Quieres aprender a sacarle el máximo provecho? Este programa es para ti. Identifica y desarrolla tus fortalezas, alcanza tus metas y diseña tu plan de vida y carrera.',
-        campi: ['AGS', 'CDMX', 'GDL']
+        campi: ['AGS', 'CDMX']
+    },
+    {
+        id: 'simulador',
+        icon: simulador,
+        title: 'Simulador de entrevista laboral',
+        subtitle: 'Prepárate para los retos intelectuales y emocionales que enfrentarás al buscar trabajo',
+        description: 'Vive un simulador de entrevista laboral para prepararte para el escenario real. ',
+        campi: ['AGS', 'CDMX']
     },
 ];
 
