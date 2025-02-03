28989dc5e1b7302be650588a20b134badc6679dd
Sergio Téllez <sergiortellez@gmail.com>
2025-02-03 17:14:30 -0600

    v.alpha @Counseling(Component), updated:     - CouneslingData: updated counseling services for AGS & GDL     - Added missing logos.     - Removed unnecessary logos.

--- src/pages/home/Experiences/buttons/Counseling/CounselingData.jsx
+++ src/pages/home/Experiences/buttons/Counseling/CounselingData.jsx
@@ -17,6 +17,7 @@ import sos from '@assets/intercampi/counseling/sos_icon.webp';
 import talent from '@assets/intercampi/counseling/talent_icon.webp';
 import simulador from '@assets/intercampi/counseling/simulador_entrevista.webp';
 import reencuadre from '@assets/intercampi/counseling/reencuadre_emocional.webp';
+import pares from '@assets/intercampi/counseling/pares.webp';
 import tutorias from '@assets/intercampi/counseling/tutorias_academicas.webp';
 import deportistas from '@assets/intercampi/counseling/deportistas.webp';
 
@@ -67,8 +68,26 @@ export const CounselingData = [
         subtitle: 'Prepárate para los retos intelectuales y emocionales que enfrentarás al buscar trabajo',
         questions: ['¿Te aterra enfrentar una entrevista laboral?', '¿Te gustaría saber cómo responder a las preguntas más difíciles?', '¿Quieres mejorar tu comunicación verbal y no verbal?'],
         description: 'Vive un simulador de entrevista laboral para prepararte para el escenario real. ',
-        campi: ['AGS', 'CDMX']
+        campi: ['CDMX']
     },
+    {
+        id: 'pares',
+        icon: pares,
+        title: 'Estudio entre pares',
+        subtitle: 'Aprende de alguien como tú, comparte tu conocimiento y así todos aprenden',
+        questions: ['¿Te cuesta trabajo entenderle a algún profesor?', '¿Te gustaría enseñar lo que sabes?', '¿Estás teniendo problemas en entender temas específicos?'],
+        description: 'Te vamos a contar uno de los secretos mejor guardados del aprendizaje: aprender de tus compañeros tiene un impacto mucho mayor que aprender de tus profesores o estudiar solo. Además, compartir lo que sabes es la mejor manera de reforzarlo.  En SMART te ofrecemos la oportunidad de participar en un grupo de estudio entre pares, donde podrás compartir tus conocimientos y aprender de los demás.',
+        campi: ['AGS']
+    },
+    {
+        id: 'spad',
+        icon: deportistas,
+        title: 'SPAD',
+        subtitle: 'Lleva el alto rendimiento de tu vida de atleta a tu vida de estudiante',
+        questions: ['¿Sabías que la misma fuerza y disciplina que te hace un gran atleta se pueden aplicar a otros aspectos de tu vida?', '¿Te has resignado a sólo ser bueno en tu deporte?', '¿No sabes cómo equilibrar tu actividad deportiva con los otros aspectos de tu vida?'],
+        description: 'EL programa SPAD es para ti. Así como un coach te ha llevado al éxito deportivo uno de nuestros expertos te ayudará a encontrar el equilibrio que estás buscando. Desde ayudarte a hacer un plan personalizado de estudio hasta equilibrar tu vida deportiva con la personal.',
+        campi: ['AGS', 'GDL']
+    }
 ];
 
 
