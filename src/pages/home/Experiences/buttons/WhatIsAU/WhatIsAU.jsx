445b93077f291ee974a7f4c52f44f4892d261196
Sergio Téllez <sergiortellez@gmail.com>
2025-02-05 19:29:04 -0600

    v.alpha @WhatIsAU(component), updated:     - Now the component has a better response to different sizes.

--- src/pages/home/Experiences/buttons/WhatIsAU/WhatIsAU.jsx
+++ src/pages/home/Experiences/buttons/WhatIsAU/WhatIsAU.jsx
@@ -58,19 +58,17 @@ export default function WhatIsAU({ className }) {
         <span>Contigo en cada paso.</span>
       </div>
       <div className={styles.container}>
-        <div className={styles.definition}>
-          <p>Sólo por ser <span>alumno</span> de la comunidad UP se te ha asignado un
-            <span>asesor</span>. El trabajo de un asesor es <span >acompañarte</span> mientras construyes tu <span>mejor versión</span>. Puede ayudarte en temas <span>personales</span>, <span>académicos</span> y <span>profesionales</span>. </p>
-          <p>Todos nuestros asesores son <b>miembros de la comunidad UP</b> que conocen a todo lo que te enfrentas como estudiante y te pueden dar solución o recomendación al respecto. Cada semestre <b>se capacitan</b> para poderte acompañar de la mejor manera, y lo más importante es
-            que
-            todo lo que platiques con ellos es <span >100% confidencial</span>.</p>
-        </div>
-        <div className={styles.image}>
-          <img
-            src={advisory}
-            alt="Advisor and student"
-          />
-        </div>
+        <img
+          className={styles.image}
+          src={advisory}
+          alt="Advisor and student"
+        />
+        <p className={styles.definition}>Sólo por ser <span>alumno</span> de la comunidad UP se te ha asignado un
+          <span>asesor</span>. El trabajo de un asesor es <span >acompañarte</span> mientras construyes tu <span>mejor versión</span>. Puede ayudarte en temas <span>personales</span>, <span>académicos</span> y <span>profesionales</span>. <br />
+          Todos nuestros asesores son <b>miembros de la comunidad UP</b> que conocen a todo lo que te enfrentas como estudiante y te pueden dar solución o recomendación al respecto. Cada semestre <b>se capacitan</b> para poderte acompañar de la mejor manera, y lo más importante es que todo lo que platiques con ellos es <span >100% confidencial</span>. </p>
+
+
+
       </div>
     </>
   )
