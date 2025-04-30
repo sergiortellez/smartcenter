f9728ef5e946d63b18505d35e3ea82bd95bc940a
sergiortellez <sergiortellez@gmail.com>
2025-04-29 19:24:32 -0600

    dev-network script adjustments.

--- dev-network.sh
+++ dev-network.sh
@@ -43,10 +43,40 @@ IS_RUNNING=$(osascript -e "$APPLESCRIPT_CHECK_APP")
 if [ "$IS_RUNNING" = "true" ]; then
     # Open URL in existing Chrome window
     osascript -e "$APPLESCRIPT_OPEN_URL"
+    
+    osascript -e '
+    tell application "Google Chrome"
+        activate
+        tell application "System Events"
+            keystroke "i" using {command down, option down}
+        end tell
+    end tell
+    '
 else
     # Start Google Chrome and open the URL
     open -a "Google Chrome" "$URL"
 fi
+
+# Open the URL in Safari
+osascript -e "
+tell application \"Safari\"
+    activate
+    tell window 1
+        set current tab to tab 1
+        set URL of current tab to \"$URL\"
+    end tell
+end tell
+"
+
+# Enter Responsive Design Mode in Safari
+osascript -e '
+tell application "Safari"
+    activate
+    tell application "System Events"
+        keystroke "r" using {control down, command down}
+    end tell
+end tell
+'
 
 # Copy the URL to the clipboard
 echo $URL | pbcopy
